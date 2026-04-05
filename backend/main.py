d1e07b7da2fda9f47069d9adb7b3cc58ebd46147
Luigi <luigivaruzza1@gmail.com>
2026-04-05 21:05:38 +0200

    updated list shopping design and be

--- backend/main.py
+++ backend/main.py
@@ -222,6 +222,7 @@ REGOLE:
 - Usa nomi semplici
 - Quantità realistiche
 - NIENTE spiegazioni
+- Qualora trovassi nomi degli ingredienti in altre lingue, traduci sempre in italiano
 - Considera eventuale contesto dato dall'utente assieme alla ricetta per adattare l'output (per esempio rispetto a varianti della ricetta o quantità)
 """
 
