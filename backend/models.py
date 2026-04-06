40c54ecc90f433a805cfa814da113d1224ade2a9
Luigi <luigivaruzza1@gmail.com>
2026-04-06 23:57:56 +0200

    render postgres implementation

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
